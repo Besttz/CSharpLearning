2dce246ba36601945e6230d77b7ea9c01f3ac7a2
Tommy <tommyzhangchina@gmail.com>
2020-12-08 21:04:25 +0800

    MergeNames

---
+++ Learning/Learning/MergeNames.cs
@@ -0,0 +1,55 @@
+﻿using System;
+namespace Learning
+{
+
+    public class MergeNames
+    {
+        public static string[] UniqueNames(string[] names1, string[] names2)
+        {
+            int duplicate = 0;
+            for (int i = 0; i < names1.Length; i++)
+            {
+                for (int j = 0; j < names2.Length; j++)
+                {
+                    if (names1[i] == names2[j])
+                    {
+                        duplicate++;
+                        break;
+                    }
+                }
+            }
+
+            int total = names1.Length + names2.Length - duplicate;
+            string[] result = new string[total];
+            for (int i = 0; i < names1.Length; i++)
+            {
+                result[i] = names1[i];
+            }
+            int index = names1.Length;
+            for (int i = 0; i < names2.Length; i++)
+            {
+                bool isDuplicated = false;
+                for (int j = 0; j < names1.Length; j++)
+                {
+                    if (result[j] == names2[i])
+                    {
+                        isDuplicated = true;
+                        break;
+                    }
+                }
+                if (!isDuplicated)
+                {
+                    result[index++] = names2[i];
+                }
+            }
+            return result;
+        }
+
+        public static void Main(string[] args)
+        {
+            string[] names1 = new string[] { "Ava", "Emma", "Olivia" };
+            string[] names2 = new string[] { "Olivia", "Sophia", "Emma" };
+            Console.WriteLine(string.Join(", ", MergeNames.UniqueNames(names1, names2))); // should print Ava, Emma, Olivia, Sophia
+        }
+    }
+}
---
+++ Learning/Learning/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace TextInput
+{
+    public class TextInput
+    {
+        protected char[] text = new char[100];
+        private int index = 0;
+        public virtual void Add(char c)
+        {
+            text[index++] = c;
+            Console.WriteLine(c);
+        }
+
+        public string GetValue()
+        {
+            return new string(text);
+        }
+    }
+
+    public class NumericInput : TextInput
+    {
+        //private char[] text = new char[100];
+        private int index = 0;
+
+        public override void Add(char c)
+        {
+            if (Char.IsDigit(c))
+            {
+                text[index++] = c;
+                Console.WriteLine(c);
+            }
+        }
+    }
+
+    class Program
+    {
+
+
+        public static void aMain(string[] args)
+        {
+            TextInput input = new NumericInput();
+            input.Add('1');
+            input.Add('a');
+            input.Add('0');
+            Console.WriteLine(input.GetValue());
+            Console.ReadLine();
+        }
+    }
+
+
+}
